7d5061fbc99ddd9e207783cf842d7f95e753ca4e
Peter Pfeufer <info@ppfeufer.de>
2016-08-31 11:10:26 +0200

    Settings updated

--- functions.php
+++ functions.php
@@ -1210,7 +1210,8 @@ function yf_get_default_background_images($withThumbnail = false, $baseClass = n
 
 		if($withThumbnail === true) {
 			foreach($images as &$image) {
-				$image = '<figure class="bg-image' . $baseClass . '"><img src="' . \get_stylesheet_directory_uri() . '/img/background/' . $image . '" style="width:100px; height:auto;" title="' . $image . '"><figcaption>' . $image . '</figcaption></figure>';
+				$imageName = ucwords(str_replace('-', ' ', preg_replace("/\\.[^.\\s]{3,4}$/", "", $image)));
+				$image = '<figure class="bg-image' . $baseClass . '"><img src="' . \get_stylesheet_directory_uri() . '/img/background/' . $image . '" style="width:100px; height:auto;" title="' . $imageName . '"><figcaption>' . $imageName . '</figcaption></figure>';
 			} // END foreach($images as &$image)
 		} // END if($withThumbnail === true)
 
