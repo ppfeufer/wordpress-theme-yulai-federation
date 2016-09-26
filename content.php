5fcbb14fe2155a3dfeaa0d00a00712ecd62d3e34
H.-Peter Pfeufer <info@ppfeufer.de>
2016-09-26 23:19:20 +0200

    code formatted

--- content.php
+++ content.php
@@ -10,13 +10,13 @@ defined('ABSPATH') or die();
 		?>
 		<a href="<?php \the_permalink(); ?>" title="<?php \the_title_attribute('echo=0'); ?>">
 			<figure class="post-loop-thumbnail">
-			<?php
-			if(\function_exists('\fly_get_attachment_image')) {
-				echo \fly_get_attachment_image(\get_post_thumbnail_id(), array(705, 395), true);
-			} else {
-				\the_post_thumbnail('post-loop-thumbnail');
-			} // END if(\function_exists('\fly_get_attachment_image'))
-			?>
+				<?php
+				if(\function_exists('\fly_get_attachment_image')) {
+					echo \fly_get_attachment_image(\get_post_thumbnail_id(), array(705, 395), true);
+				} else {
+					\the_post_thumbnail('post-loop-thumbnail');
+				} // END if(\function_exists('\fly_get_attachment_image'))
+				?>
 			</figure>
 		</a>
 		<?php
